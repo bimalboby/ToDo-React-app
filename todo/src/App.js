063d53e5a03b9f2490a3cf0e3d9ea76ae9f065a6
Bimal Boby <bimalboby007@gmail.com>
2021-05-28 13:05:35 -0800

    ready to start project, files ready

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
+import Header from './components/Header'
 
 function App() {
+  var data="data large"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='hello'>Hello world </h1>
+      <p>hey man</p>
+      <Header sent={data}/>
+     
     </div>
+
+
+
+
   );
 }
 
 export default App;
+
---
+++ todo/src/components/Header.js
@@ -0,0 +1,6 @@
+function Header(props){
+
+    return (<h1>Header {props.sent}</h1>)
+  }
+  export default  Header
+  
